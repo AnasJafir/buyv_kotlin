09c909b673691d34897430d634ee364564c144bd
AnasJafir <dr.anas.jafir@gmail.com>
2026-03-23 18:54:44 +0100

    chore(backend): update CORS settings for flutter web

--- buyv_backend/app/main.py
+++ buyv_backend/app/main.py
@@ -86,6 +86,7 @@ else:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Restricted to specific origins
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
